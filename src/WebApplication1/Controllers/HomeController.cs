2f5cfe56856a342a23c9db49957c2b30b773e884
Will <doggy.huang@gmail.com>
2015-09-16 00:22:00 +0800

    Initial commit

---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.OptionsModel;
+using Microsoft.Framework.Caching.Memory;
+using System.Text;
+using Microsoft.AspNet.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyOptions Options { get; }
+        IMemoryCache Cache { get; }
+
+        public HomeController(IOptions<MyOptions> optionsAccessor, IMemoryCache cache)
+        {
+            Options = optionsAccessor.Options;
+            Cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            Context.Session.SetString("Message", "Hello Session " + DateTime.Now);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Session: " + Context.Session.GetString("Message");
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ src/WebApplication1/NewFolder/MyOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class AppSettings
+    {
+        public string SMTPIP { get; set; }
+        public string SMTPID { get; set; }
+        public string SMTPPW { get; set; }
+    }
+    public class MyOptions
+    {
+        public string ConnectionString { get; set; }
+
+        public AppSettings AppSettings { get; set; }
+    }
+}
